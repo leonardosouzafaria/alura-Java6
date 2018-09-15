076648faf6829e7815c8669b3121c6f0ca6ff8e9
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-09-15 15:16:37 -0300

    Mãos na massa: Wrappers

---
+++ bytebank-herado-conta/src/br/com/bytebank/banco/teste/util/TesteWrapperInteger.java
@@ -0,0 +1,42 @@
+package br.com.bytebank.banco.teste.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteWrapperInteger {
+	
+	public static void main(String[] args) {
+		
+		Integer idadeRef = 29; //autoboxinng, é criado um objeto do tipo Integer
+        int primitivo = new Integer(21); //que locura!! unboxing
+
+        List<Integer> lista = new ArrayList<>();
+        lista.add(idadeRef); //ok
+        lista.add(primitivo); //autoboxing
+
+        int i1 = lista.get(0);  //unboxing
+        Integer i2 = lista.get(1);  //ok
+
+        System.out.println(i1);  //29
+        System.out.println(i2); //21
+
+        Integer valorRef = Integer.valueOf(33);
+        int valorPrimitivo =  valorRef.intValue();
+        
+        System.out.println(valorPrimitivo);
+		
+        Integer iParseado1 = Integer.valueOf("42");
+        int iParseado2 = Integer.parseInt("44");	
+        
+        System.out.println(iParseado1); //42
+        System.out.println(iParseado2); //44
+        
+        //verificando constantes da classe Integer
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.BYTES);
+	}
+
+}
