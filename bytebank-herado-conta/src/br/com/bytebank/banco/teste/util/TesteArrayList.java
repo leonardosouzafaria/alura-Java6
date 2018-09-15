806cc683e922739db0540228bfef3a3d273a9e78
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-09-15 14:31:19 -0300

    Maos na massa: Vector e Collection

--- bytebank-herado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
+++ bytebank-herado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
@@ -1,6 +1,9 @@
 package br.com.bytebank.banco.teste.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Vector;
 
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
@@ -44,6 +47,13 @@ public class TesteArrayList {
 		for(Object obj : lista){
 			System.out.println(ref);	
 		}
+		
+		List<Conta> listaVector = new Vector<Conta>();
+		
+		Collection<Conta> testeCollection = new Vector<Conta>();
+		
+		
+		
 	}
 
 }
